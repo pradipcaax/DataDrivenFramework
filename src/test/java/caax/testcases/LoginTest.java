e6d23c4be13094ccca41345c703941615550784c
pradipcaax <39522507+pradipcaax@users.noreply.github.com>
2019-01-29 13:40:17 +0530

    final Data Driven

--- src/test/java/caax/testcases/LoginTest.java
+++ src/test/java/caax/testcases/LoginTest.java
@@ -12,6 +12,7 @@ public class LoginTest extends TestBase {
          driver.findElement(By.xpath(OR.getProperty("userId"))).sendKeys("pradip.caax@gmail.com");
          driver.findElement(By.xpath(OR.getProperty("password"))).sendKeys("pradip123");
          driver.findElement(By.xpath(OR.getProperty("login_btn"))).click();
+         //pradip
 
 
 
